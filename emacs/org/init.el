d829adc0bb9212c734fd7981411dd6bbab09d739
John De Armas <dearmas.john1996@gmail.com>
2021-03-02 20:28:25 -0700

    add a lisp function to quickly create directories with org-links

--- emacs/org/init.el
+++ emacs/org/init.el
@@ -40,3 +40,37 @@
 
 (require 'ox-extra)
 (ox-extras-activate '(ignore-headlines))
+
+(defun org-create-link()
+  "Create a directory link, that makes the directory and index file."
+  (interactive)
+  (setq text (thing-at-point 'filename))
+(condition-case nil
+    (org-open-at-point)
+  (error 
+  (when (string= (substring text -1 nil) "/")
+        (setq pathname (concat default-directory text "index.org"))
+        (unless (file-directory-p text)
+          (make-directory text)
+        )
+        (unless (file-exists-p pathname)
+          (make-empty-file pathname)
+        )
+        (append-to-file (format "#+TITLE: %s" text) nil pathname)
+        ;;(org-insert-link nil pathname text)
+        (setq bounds (bounds-of-thing-at-point 'filename))
+        (setq pos1 (car bounds))
+        (setq pos2 (cdr bounds))
+        (setq mything (buffer-substring-no-properties pos1 pos2))
+        (delete-region pos1 pos2)
+        (insert (format "[[%s][%s]]" pathname text))
+        )
+  )
+  )
+)
+
+
+(eval-after-load 'org-mode
+  '(define-key evil-normal-state-map (kbd "RET") 'org-create-link))
+
+  
